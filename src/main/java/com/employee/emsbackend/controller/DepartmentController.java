226166336deb60d05c191c23523a66b2b73f623d
Taniya <taniyapushey@gmail.com>
2024-03-20 12:39:34 +0530

    Role based authentication

--- src/main/java/com/employee/emsbackend/controller/DepartmentController.java
+++ src/main/java/com/employee/emsbackend/controller/DepartmentController.java
@@ -4,6 +4,7 @@ import com.employee.emsbackend.dto.DepartmentDto;
 import com.employee.emsbackend.service.DepartmentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,6 +19,7 @@ public class DepartmentController {
         this.departmentService = departmentService;
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public ResponseEntity<DepartmentDto> createDepartment(@RequestBody DepartmentDto departmentDto) {
         DepartmentDto createdDepartment=departmentService.createDepartment(departmentDto);
@@ -34,6 +36,7 @@ public class DepartmentController {
         return ResponseEntity.ok(departmentService.getAllDepartments());
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("{id}")
     public ResponseEntity<DepartmentDto> updateDepartment(@RequestBody DepartmentDto departmentDto,
                                           @PathVariable("id") Long departmentId) {
@@ -41,6 +44,7 @@ public class DepartmentController {
         return ResponseEntity.ok(updatedDepartment);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("{id}")
     public ResponseEntity<String> deleteDepartment(@PathVariable("id") Long departmentId) {
         departmentService.deleteDepartment(departmentId);
--- src/main/java/com/employee/emsbackend/controller/EmployeeController.java
+++ src/main/java/com/employee/emsbackend/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import com.employee.emsbackend.dto.EmployeeDto;
 import com.employee.emsbackend.service.EmployeeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,6 +20,7 @@ public class EmployeeController {
         this.employeeService = employeeService;
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto){
         return new ResponseEntity<>(employeeService.createEmployee(employeeDto),HttpStatus.CREATED);
@@ -34,11 +36,13 @@ public class EmployeeController {
         return ResponseEntity.ok(employeeService.getEmployeeById(id));
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}")
     public ResponseEntity<EmployeeDto> updateEmployee(@RequestBody EmployeeDto employeeDto,@PathVariable long id) {
         return new ResponseEntity<>(employeeService.updateEmployee(employeeDto,id),HttpStatus.OK);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("{id}")
     public ResponseEntity<String> deleteEmployee(@PathVariable long id) {
         employeeService.deleteEmployee(id);
